03689c8afca9f4969610dec7717cebf6c626cfe7
superwhiskers <whiskerdev@protonmail.com>
2019-04-06 00:32:11 -0500

    fix stdbytebuffer benchmark

--- stdbytebuffer_test.go
+++ stdbytebuffer_test.go
@@ -9,11 +9,17 @@ func BenchmarkStdByteBufferWrite(b *testing.B) {
 
 	b.ReportAllocs()
 
-	buf := bytes.NewBuffer([]byte{0x00, 0x00, 0x00, 0x00})
+	buf := bytes.NewBuffer([]byte{})
 
+	var err error
 	for n := 0; n < b.N; n++ {
 
-		buf.Write([]byte{0x01, 0x02})
+		_, err = buf.Write([]byte{0x01, 0x02})
+		if err != nil {
+
+			b.Fatal(err)
+
+		}
 		buf.Reset() // needs to be done because we'll overwrite otherwise
 
 	}
@@ -24,20 +30,23 @@ func BenchmarkStdByteBufferRead(b *testing.B) {
 
 	b.ReportAllocs()
 
-	buf := bytes.NewBuffer([]byte{0x00, 0x00, 0x00, 0x00})
-
-	out := []byte{0x00, 0x00}
+	buf := bytes.NewBuffer([]byte{0x00, 0x00})
 
 	var err error
 	for n := 0; n < b.N; n++ {
 
-		_, err = buf.Read(out)
+		_, err = buf.ReadByte()
 		if err != nil {
 
 			b.Fatal(err)
 
 		}
-		buf.Reset()
+		err = buf.UnreadByte()
+		if err != nil {
+
+			b.Fatal(err)
+
+		}
 
 	}
 
