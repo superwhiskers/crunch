4105190e855ef339ddc1d89fa90489254d790064
superwhiskers <whiskerdev@protonmail.com>
2021-01-22 01:46:30 -0600

    crunch: attempt at making imports easier

---
+++ lib.go
@@ -0,0 +1,12 @@
+/*
+
+crunch - utilities for taking bytes out of things
+copyright (c) 2019-2021 superwhiskers <whiskerdev@protonmail.com>
+
+this source code form is subject to the terms of the mozilla public
+license, v. 2.0. if a copy of the mpl was not distributed with this
+file, you can obtain one at http://mozilla.org/MPL/2.0/.
+
+*/
+
+package crunch
