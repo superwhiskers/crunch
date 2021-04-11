79d0fc814958d366ac598d3d00924e6045fdfbfd
superwhiskers <whiskerdev@protonmail.com>
2021-04-11 13:26:49 -0500

    crunch: update go version, add go.mod files for each major version, and ensure tests pass

--- shell.nix
+++
@@ -1,9 +0,0 @@
-{ sources ? import ./nix/sources.nix, pkgs ? import ./nix { inherit sources; } }:
-
-pkgs.mkShell {
-  name = "crunch-shell";
-
-  buildInputs = with pkgs; [
-    go
-  ];
-}
--- v2/go.mod
+++ v2/go.mod
@@ -1,4 +1,4 @@
-module github.com/superwhiskers/crunch/v3
+module github.com/superwhiskers/crunch/v2
 
 go 1.16
 
