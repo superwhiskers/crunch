7ad82f4e8557613ee7b3adacb55c71cf94bc38bd
superwhiskers <whiskerdev@protonmail.com>
2023-01-13 20:55:43 -0600

    crunch: make the license notice verbatim

--- generation/main.go
+++ generation/main.go
@@ -1,3 +1,14 @@
+/*
+
+crunch - utilities for taking bytes out of things
+Copyright (c) 2019-2020 superwhiskers <whiskerdev@protonmail.com>
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+*/
+
 package main
 
 import (
